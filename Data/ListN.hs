208da7d10fe4f6628f575e8e68f1e6d988f08a6e
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2011-02-20 20:42:37 -0800

    Define listIx, and identify some properties that we are missing.

--- Data/ListN.hs
+++ Data/ListN.hs
@@ -1,27 +1,67 @@
-{-# LANGUAGE TypeNaturals, GADTs #-}
+{-# LANGUAGE TypeNaturals, GADTs, FlexibleContexts #-}
 module Data.ListN where
 
 import GHC.TypeNats
 
 --------------------------------------------------------------------------------
 
+class Listable list where
+  toList :: list a -> [a]
+
+infixr :-
+data Stream a       = a :- Stream a
+
+unfold :: (b -> (a,b)) -> b -> Stream a
+unfold f b = let (a,b') = f b in a :- unfold f b'
+
+instance Listable Stream where
+  toList (a :- as)  = a : toList as
+
+instance Functor Stream where
+  fmap f = unfold (\(a :- as) -> (f a, as))
+
+instance Show a => Show (Stream a) where
+  show = show . toList
+
+splitStream :: Nat n -> Stream a -> (List n a, Stream a)
+splitStream n xs = def (natToNatU n) xs
+  where
+  def :: NatU n -> Stream a -> (List n a, Stream a)
+  def Zero xs            = (Nil, xs)
+  def (Succ n) (a :- as) = let (xs,ys) = def n as
+                           in (a :> xs, ys)
+
+zipStreamWith :: (a -> b -> c) -> Stream a -> Stream b -> Stream c
+zipStreamWith f (a :- as) (b :- bs) = f a b :- zipStreamWith f as bs
+
+
+splitStreamI :: NatI n => Stream a -> (List n a, Stream a)
+splitStreamI = splitStream nat
+
+nats :: Stream Int
+nats = 0 :- fmap (1+) nats
+
+repl :: a -> Stream a
+repl a = unfold (\a -> (a,a)) a
+
+cyc :: (1 <= n) => List n a -> Stream a
+cyc xs = go (SomeList xs)
+  where
+  go (SomeList Nil)        = go (SomeList xs)
+  go (SomeList (a :> as))  = a :- go (SomeList as)
+
+infixr :>
 data List :: Nat -> * -> * where
   Nil  :: List 0 a
   (:>) :: a -> List n a -> List (n + 1) a
 
-toList :: List n a -> [a]
-toList Nil        = []
-toList (x :> xs)  = x : toList xs
+data SomeList :: * -> * where
+  SomeList :: List n a -> SomeList a
 
-fromList :: Nat n -> [a] -> List n a
-fromList n xs = def (natToNatU n) xs
-  where
-  def :: NatU n -> [a] -> List n a
-  def Zero _             = Nil
-  def (Succ n) ~(a:as)   = a :> def n as
+instance Listable (List n) where
+  toList Nil        = []
+  toList (x :> xs)  = x : toList xs
 
-fromListI :: NatI n => [a] -> List n a
-fromListI = fromList nat
 
 instance Show a => Show (List n a) where
   show = show . toList
@@ -33,17 +73,6 @@ join2 (x :> xs) ys  = x :> join2 xs ys
 joinMany :: List m (List n a) -> List (m * n) a
 joinMany Nil          = Nil
 joinMany (xs :> yss)  = join2 xs (joinMany yss)
-
-repl :: Nat n -> a -> List n a
-repl x a = def (natToNatU x) a
-  where
-  def :: NatU n -> a -> List n a
-  def Zero _      = Nil
-  def (Succ m) a  = a :> def m a
-
-replI :: NatI n => a -> List n a
-replI = repl nat
-
 
 split2 :: Nat m -> List (m + n) a -> (List m a, List n a)
 split2 = def . natToNatU
@@ -66,7 +95,63 @@ splitMany m n xs = def (natToNatU m) n xs
 splitManyI :: (NatI m, NatI n) => List (m * n) a -> List m (List n a)
 splitManyI xs = splitMany nat nat xs
 
+zipListWith :: (a -> b -> c) -> List n a -> List n b -> List n c
+zipListWith f Nil Nil             = Nil
+zipListWith f (x :> xs) (y :> ys) = f x y :> zipListWith f xs ys
 
 example :: List 10 (List 3 Int)
-example = splitManyI (fromListI [0..])
+example = splitManyI $ fst $ splitStreamI nats
+
+zipListStream :: NatI n => (a -> b -> c) -> Stream a -> List n b -> List n c
+zipListStream f l xs = zipListWith f (fst $ splitStreamI l) xs
+
+listHead :: (1 <= n) => List n a -> a
+listHead (a :> _) = a
+
+listTail :: List (n + 1) a -> List n a
+listTail (_ :> as) = as
+
+listLast :: (1 <= n) => List n a -> a
+listLast (a :> Nil)         = a
+listLast (_ :> xs@(_ :> _)) = listLast xs
+
+{- TODO:
+m = n1 + 1
+n = n2 + 1
+m + 1 <= n
+===>
+m <= n2
+
+i.e.
+
+m + 1 <= n2 + 1
+
+
+listIx :: ((m + 1) <= n) => NatU m -> List n a -> a
+listIx Zero (a :> _)      = a
+listIx (Succ n) (_ :> xs) = listIx n xs
+-}
+
+{-
+1 <= n
+m = (n1 + 1)
+m + n = n2 + 1
+==>
+n2 = n1 + n0
+
+(n1 + 1) + n = n2 + 1
+==>
+n2 = n1 + n
+
+listIx2 :: (1 <= n) => NatU m -> List (m + n) a -> a
+listIx2 Zero (a :> _)      = a
+listIx2 (Succ n) (_ :> xs) = listIx2 n xs
+-}
+
+listIx :: (1 <= n) => Nat m -> List (m + n) a -> a
+listIx n xs = listHead $ snd $ split2 n xs
+
+
+
+
 
